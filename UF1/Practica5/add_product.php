4ca175dc0865d1bafd8f1182cc497581fd103ce3
FHP187 <gjimenez@jaumebalmes.net>
2022-10-18 20:04:54 +0200

    conexion practica5

---
+++ UF1/Practica5/add_product.php
@@ -0,0 +1,63 @@
+<?php
+
+include 'db_connection_pdo.php';
+
+$productSaved = FALSE;
+
+if (isset($_POST['submit'])) {
+
+    $productName = isset($_POST['name']) ? $_POST['name'] : '';
+    $productDescription = isset($_POST['description']) ? $_POST['description'] : '';
+    $productPrice = isset($_POST['price']) ? $_POST['price'] : 0;
+    $productQuantity = isset($_POST['quantity']) ? $_POST['quantity'] : 0;
+
+
+    /*
+     * Validate posted values.
+     */
+    if (empty($productName)) {
+        $errors[] = 'Please provide a product name.';
+    }
+
+    if (empty($productDescription)) {
+        $errors[] = 'Please provide a description.';
+    }
+
+    if (empty($productPrice)) {
+        $errors[] = 'Please provide a description.';
+    }
+
+    if ($productQuantity == 0) {
+        $errors[] = 'Please provide the quantity.';
+    }
+
+    if (!isset($errors)) {
+
+        $sql = 'INSERT INTO products (
+                    name,
+                    description,
+                    price,
+                    quantity
+                ) VALUES (
+                    ?, ?, ?,?
+                )';
+
+        $statement = $connexio->prepare($sql);
+
+
+        $statement->bind_param('sis', $productName, $productDescription,$productPrice, $productQuantity);
+
+
+        $statement->execute();
+
+
+        $lastInsertId = $connexio->insert_id;
+
+
+        $statement->close();
+
+        $productSaved = TRUE;
+
+    }
+}
+?>
--- UF1/Practica5/db_connection_pdo.php
+++ UF1/Practica5/db_connection_pdo.php
@@ -1,15 +1,12 @@
 <?php
 
+// parametros
+$dsn = 'mysql:host=localhost;port=3306;dbname=dbproducts';
+$username = 'root';
+$password = '';
+$options = [];
 
-
-
-
-
-
-
-
-
-
+$connexio = new PDO($dsn, $username, $password, $options);
 
 
 ?>
