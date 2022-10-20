b9c916ea2252e5a77127bced3f32e6df1359c65f
FHP187 <gjimenez@jaumebalmes.net>
2022-10-20 20:56:44 +0200

    edit add_product modificado

--- UF1/Practica5_part2/add_product.php
+++ UF1/Practica5_part2/add_product.php
@@ -2,7 +2,7 @@
 
 try {
     //Connexió a la BBDD
-    $myCon = new PDO('mysql:host=localhost; dbname=products', 'test', 'test');
+    $myCon = new PDO('mysql:host=localhost:3336; dbname=products', 'test', 'test');
     //Creem la consulta sql
     $sql ="SELECT * FROM product";
 
