ccfc7e631fd035a3b17cfd4fb92793998e7ec533
RaulRufoEnciso <rrufo19@jaumebalmes.net>
2022-10-20 19:37:31 +0200

    Connexió a la bbdd amb PDO.

---
+++ UF1/Practica5_part2/db_connexion_pdo.php
@@ -0,0 +1,14 @@
+<?php
+//db
+try {
+    //Connexió a la BBDD
+    $myCon = new PDO('mysql:host=localhost; dbname=products', 'root', '');
+    //Creem la consulta sql
+    $sql ="SELECT * FROM product";
+
+} catch (PDOException $e) {
+    echo "error de connexió: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+?>
