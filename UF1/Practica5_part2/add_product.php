0d85fa532e64c44c372958f61c62ce4ea1f3b380
FHP187 <gjimenez@jaumebalmes.net>
2022-10-20 20:41:10 +0200

    edit add_product

--- UF1/Practica5_part2/add_product.php
+++ UF1/Practica5_part2/add_product.php
@@ -1,10 +1,46 @@
 <?php
 
+try {
+    //Connexió a la BBDD
+    $myCon = new PDO('mysql:host=localhost; dbname=products', 'test', 'test');
+    //Creem la consulta sql
+    $sql ="SELECT * FROM product";
 
-
+} catch (PDOException $e) {
+    echo "error de connexió: " . $e->getMessage() . "<br/>";
+    die();
+}
 
 
 
 
 
 ?>
+
+
+
+<!-- SECCIÓ PER AFEGIR PRODUCTES -->
+<div class="container p-4">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card card-body">
+                <!-- A través del mètode POST li enviem les dades del formulari a l'arxiu add_product.php -->
+                <form action="add_product.php" method="POST">
+                    <div class=form-group>
+                        <input type="text" name="name" class="form-control" placeholder="Name" autofocus>
+                    </div>
+                    <div class=form-group>
+                        <textarea name="description" rows="3" class="form-control" placeholder="Description"></textarea>
+                    </div>
+                    <div class=form-group>
+                        <input type="text" name="price" class="form-control" placeholder="price">
+                    </div>
+                    <div class=form-group>
+                        <input type="text" name="quantity" class="form-control" placeholder="Quantity">
+                    </div>
+                    <input type="submit" class="btn btn-success btn-block" name="add_product" value="+ Producte">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
